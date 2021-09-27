aeae4a9597bf78d53809f5513e81c75b1861ec8a
abhinav tiwari <you@example.com>
2021-09-27 18:29:19 +0530

    Inital commit

---
+++ nodeServer/index.js
@@ -0,0 +1,26 @@
+//Node server for handling socket.io connections.
+// Access-Control-Allow-Origin : "https://localhost:8000/index.html" ;
+//const io = require('socket.io')(8000);
+const io = require('socket.io')(8000, {
+    cors: {
+        origin: '*',
+    }
+});
+
+const users = {};
+io.on('connection', socket =>{
+    socket.on('new-user-joined', name => {
+        users[socket.id] = name;
+        console.log("new user", name);
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive',{message: message, name: users[socket.id]})
+    });
+
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left',  users[socket.id]);
+        delete users[socket.id];
+    });
+})
